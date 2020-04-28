65f4a4cf09fbeeafb27b4f7f31026159dce6a0e8
radu.memetea <radumemetea@gmail.com>
2020-04-29 01:12:29 +0300

    Made a release version

--- FrontEnd/Startup.cs
+++ FrontEnd/Startup.cs
@@ -43,7 +43,7 @@ namespace FrontEnd
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
